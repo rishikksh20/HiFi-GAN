2957f6d7b8b9fc6fa3f1c3e89a2a4d63719a2541
rishikksh20 <rishikksh20@gmail.com>
2020-10-14 18:12:23 +0530

    :star: Add MPD

--- model/mpd.py
+++ model/mpd.py
@@ -1,33 +1,22 @@
 from torch import nn
-import torch.nn.functional as F
+from model.period_discriminator import PeriodDiscriminator
+
 
 class MPD(nn.Module):
+    def __init__(self, periods=[2, 3, 5, 7, 11], segment_length=16000):
+        super(MPD, self).__init__()
+        self.mpd1 = PeriodDiscriminator(periods[0])
+        self.mpd2 = PeriodDiscriminator(periods[1])
+        self.mpd3 = PeriodDiscriminator(periods[2])
+        self.mpd4 = PeriodDiscriminator(periods[3])
+        self.mpd5 = PeriodDiscriminator(periods[4])
 
-  def __init__(self, periods, segment_length=16000):
-    super(MPD, self).__init__()
-    layer = []
-    self.periods = periods
-    self.pad = segment_length%periods
-    inp = 1
-    for l in range(4):
-       
-      out = int(2**(5+l+1))
-      layer += [
-                nn.Conv2d(inp, out, kernel_size=(5, 1), stride=(3, 1)),
-                nn.LeakyReLU(0.2)
-      ]
-      inp = out
-    self.layer = nn.Sequential(*layer)
-    self.output = nn.Sequential(
-        nn.Conv2d(out, 1024, kernel_size=(5, 1)),
-        nn.LeakyReLU(0.2),
-        nn.Conv2d(1024, 1, kernel_size=(3, 1))
-        )
-  
-  def forward(self, x):
-    batch_size = x.shape[0]
-    x = F.pad(x, (0, self.pad))
-    y = x.view(batch_size,-1, self.periods).contiguous()
-    y = y.unsqueeze(1)
-    out1 = self.layer(y)
-    return self.output(out1)
+    def forward(self, x):
+        out1 = self.mpd1(x)
+        out2 = self.mpd2(x)
+        out3 = self.mpd3(x)
+        out4 = self.mpd4(x)
+        out5 = self.mpd5(x)
+        return out1, out2, out3, out4, out5
+
+
---
+++ model/period_discriminator.py
@@ -0,0 +1,33 @@
+from torch import nn
+import torch.nn.functional as F
+
+class PeriodDiscriminator(nn.Module):
+
+  def __init__(self, period, segment_length=16000):
+    super(PeriodDiscriminator, self).__init__()
+    layer = []
+    self.period = period
+    self.pad = segment_length%period
+    inp = 1
+    for l in range(4):
+       
+      out = int(2**(5+l+1))
+      layer += [
+                nn.utils.weight_norm(nn.Conv2d(inp, out, kernel_size=(5, 1), stride=(3, 1))),
+                nn.LeakyReLU(0.2)
+      ]
+      inp = out
+    self.layer = nn.Sequential(*layer)
+    self.output = nn.Sequential(
+        nn.utils.weight_norm(nn.Conv2d(out, 1024, kernel_size=(5, 1))),
+        nn.LeakyReLU(0.2),
+        nn.utils.weight_norm(nn.Conv2d(1024, 1, kernel_size=(3, 1)))
+        )
+  
+  def forward(self, x):
+    batch_size = x.shape[0]
+    x = F.pad(x, (0, self.pad))
+    y = x.view(batch_size,-1, self.periods).contiguous()
+    y = y.unsqueeze(1)
+    out1 = self.layer(y)
+    return self.output(out1)
